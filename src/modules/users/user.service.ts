5d70700cba6ce789c4836c6dca50efb3868d02c4
Mintu Sikder <mintusikder15@gmail.com>
2026-04-24 23:00:54 +0600

    added password bcrypt

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,12 +1,15 @@
 import { pool } from "../../database/db";
-
-const createUserIntoDb = async(payload: Record<string,undefined>) =>{
+import bcrypt from "bcryptjs"
+const createUserIntoDb = async(payload: Record<string,unknown>) =>{
   const { name, email, password } = payload;
+
+  const hashPassword = await bcrypt.hash(password as string ,10)
+
     const result = await pool.query(
       `
     INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING *
     `,
-      [name, email, password],
+      [name, email, hashPassword],
     );
     return result;
 }
